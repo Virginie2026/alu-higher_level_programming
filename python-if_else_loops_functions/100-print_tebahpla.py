19de83b3394b3c11213b884cf98489727e66d35f
Virginie2026 <s.millimoun@alustudent.com>
2026-06-26 09:48:42 -0700

    Add solution for task 100

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-print("".join(chr(i).lower() if (122 - i) % 2 == 0 else chr(i).upper() for i in range(122, 96, -1)), end="")
+print("".join("{:c}".format(i).lower() if (122 - i) % 2 == 0 else "{:c}".format(i).upper() for i in range(122, 96, -1)), end="")
