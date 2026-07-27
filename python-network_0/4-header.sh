8621f718744dffc571d0dfa7b76e72a61b6e863a
Virginie2026 <s.millimoun@alustudent.com>
2026-07-27 10:14:51 +0000

    Fix task 4 header syntax

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a custom header and displays the response body
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to a URL with a header variable
+curl -s -H "X-HolbertonSchool-User-Id: 98" $1
