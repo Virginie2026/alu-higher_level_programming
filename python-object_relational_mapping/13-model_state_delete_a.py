bd4188b04b82f67a7a65d57e76fc5857ffb94651
Virginie2026 <s.millimoun@alustudent.com>
2026-07-30 07:14:27 -0700

    Task 13: Fix case sensitive deletion of letter 'a'

--- python-object_relational_mapping/13-model_state_delete_a.py
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -21,13 +21,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    states_to_delete = (
-        session.query(State)
-        .filter(State.name.like('%a%'))
-        .all()
-    )
-
-    for state in states_to_delete:
+    states = session.query(State).all()
+    for state in states:
         if 'a' in state.name:
             session.delete(state)
 
