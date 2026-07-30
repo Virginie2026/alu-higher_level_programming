39345d221fce96126f55deb51d159c13d959936b
Virginie2026 <s.millimoun@alustudent.com>
2026-07-30 05:42:59 -0700

    Task 11: Add size getter and setter to Square

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module defining the Square class inheriting from Rectangle."""
+"""Module defining the Square class with size getter and setter."""
 from models.rectangle import Rectangle
 
 
@@ -9,6 +9,17 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square instance using Rectangle constructor."""
         super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """Get the size of the Square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the Square (updates width and height)."""
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Return [Square] (<id>) <x>/<y> - <size>."""
