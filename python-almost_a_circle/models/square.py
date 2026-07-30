e4d610fd3f774d5780f5589f2527a876ad904e5a
Virginie2026 <s.millimoun@alustudent.com>
2026-07-30 05:40:56 -0700

    Task 10: Create Square class inheriting from Rectangle

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -1,61 +1,17 @@
 #!/usr/bin/python3
-"""This module defines the Square class, inheriting from Rectangle."""
+"""Module defining the Square class inheriting from Rectangle."""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Represent a square, built on top of the Rectangle class."""
+    """Square class inheriting from Rectangle."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialize a new Square.
-
-        Args:
-            size (int): the size of the square.
-            x (int): the horizontal offset of the square.
-            y (int): the vertical offset of the square.
-            id (int, optional): the id of the square.
-        """
+        """Initialize a new Square instance using Rectangle constructor."""
         super().__init__(size, size, x, y, id)
 
-    @property
-    def size(self):
-        """Retrieve the size of the square."""
-        return self.width
-
-    @size.setter
-    def size(self, value):
-        """Set the size of the square (maps to both width and
-        height).
-        """
-        self.width = value
-        self.height = value
-
     def __str__(self):
-        """Return the string representation of the square."""
+        """Return [Square] (<id>) <x>/<y> - <size>."""
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.width)
-
-    def update(self, *args, **kwargs):
-        """Update the square's attributes.
-
-        Args:
-            *args: new attribute values in the order id, size, x, y.
-            **kwargs: new attribute values as key/value pairs, used
-                only if *args is empty.
-        """
-        if args:
-            attrs = ["id", "size", "x", "y"]
-            for attr, value in zip(attrs, args):
-                setattr(self, attr, value)
-        else:
-            for key, value in kwargs.items():
-                setattr(self, key, value)
-
-    def to_dictionary(self):
-        """Return the dictionary representation of the square."""
-        return {
-            "id": self.id,
-            "size": self.size,
-            "x": self.x,
-            "y": self.y,
-        }
+            self.id, self.x, self.y, self.width
+        )
