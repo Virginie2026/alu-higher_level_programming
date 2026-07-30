949c134e6737bec2d0d211ab1a1b5d1d3fd8003a
Virginie2026 <s.millimoun@alustudent.com>
2026-07-30 05:18:13 -0700

    Add Rectangle class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -7,7 +7,7 @@ class Rectangle(Base):
     """Rectangle class inheriting from Base."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle instance."""
+        """Initialize Rectangle instance."""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -16,56 +16,40 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Get the width of the rectangle."""
+        """Get width."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Set the width of the rectangle with validation."""
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
+        """Set width."""
         self.__width = value
 
     @property
     def height(self):
-        """Get the height of the rectangle."""
+        """Get height."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Set the height of the rectangle with validation."""
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
+        """Set height."""
         self.__height = value
 
     @property
     def x(self):
-        """Get the x coordinate of the rectangle."""
+        """Get x."""
         return self.__x
 
     @x.setter
     def x(self, value):
-        """Set the x coordinate of the rectangle with validation."""
-        if type(value) is not int:
-            raise TypeError("x must be an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
+        """Set x."""
         self.__x = value
 
     @property
     def y(self):
-        """Get the y coordinate of the rectangle."""
+        """Get y."""
         return self.__y
 
     @y.setter
     def y(self, value):
-        """Set the y coordinate of the rectangle with validation."""
-        if type(value) is not int:
-            raise TypeError("y must be an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
+        """Set y."""
         self.__y = value
