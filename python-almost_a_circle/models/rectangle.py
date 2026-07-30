9ba2eb59405927676174e4d5392f6e15099cd520
Virginie2026 <s.millimoun@alustudent.com>
2026-07-30 05:38:54 -0700

    Task 9: Support **kwargs in Rectangle update method

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module defining the Rectangle class with update method using *args."""
+"""Module defining Rectangle class with update (*args, **kwargs)."""
 from models.base import Base
 
 
@@ -86,9 +86,14 @@ class Rectangle(Base):
             self.id, self.x, self.y, self.width, self.height
         )
 
-    def update(self, *args):
-        """Assign an argument to each attribute using position."""
-        attrs = ["id", "width", "height", "x", "y"]
-        for i, arg in enumerate(args):
-            if i < len(attrs):
-                setattr(self, attrs[i], arg)
+    def update(self, *args, **kwargs):
+        """Assign arguments to attributes (*args take precedence)."""
+        if args and len(args) != 0:
+            attrs = ["id", "width", "height", "x", "y"]
+            for i, arg in enumerate(args):
+                if i < len(attrs):
+                    setattr(self, attrs[i], arg)
+        elif kwargs and len(kwargs) != 0:
+            for key, value in kwargs.items():
+                if hasattr(self, key):
+                    setattr(self, key, value)
